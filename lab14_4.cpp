fdbd92fb6566b6a6937f351a4333a48bf0dc3531
mook <keettamook_k@cmu.ac.th>
2020-02-25 15:37:06 +0700

    finish 4

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -19,3 +19,36 @@ int main(){
 }
 
 //Write definition of shuffle() here 
+
+void shuffle(int &x,int &y, int &z, int &k){
+	int a[4]={x,y,z,k};
+	int q,w,e,r;
+
+	q = rand()%4;
+	x = a[q];
+	while(true){
+		w = rand()%4;
+		if(w!=q){
+			y = a[w];
+			break;
+		}
+		
+	}
+	while(true){
+		e = rand()%4;
+		if(e!=q && e!=w){
+			z = a[e];
+			break;
+		}
+		
+	}
+	while(true){
+		r = rand()%4;
+		if(r!=q && r!=w && r!=e){
+			k = a[r];
+			break;
+		}
+		
+	}
+
+}
